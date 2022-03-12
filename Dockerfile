aaa80f2083c7428397975968c82c7cbc88d900a9
ubuntu <ubuntu@localhost.localdomain>
2022-03-12 15:25:38 +0800

    first

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu:latest
+MAINTAINER yhl
+ADD install.sh /
+RUN chmod u+x /install.sh
+RUN /install.sh
+ENV PATH /root/miniconda3/bin:$PATH
+CMD ["ipython"]
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+apt-get clean
+
+wget -c https://repo.anaconda.com/miniconda/Miniconda3-py39_4.11.0-Linux-x86_64.sh
+bash Miniconda3-py39_4.11.0-Linux-x86_64.sh -b
+rm -rf Miniconda3-py39_4.11.0-Linux-x86_64.sh
+export PATH=/root/miniconda3/bin:$PATH
