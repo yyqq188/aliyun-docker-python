a16f783750fafed62af4f517ce062d1ce9c16451
ubuntu <ubuntu@localhost.localdomain>
2022-03-12 19:52:27 +0800

    second

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ wget -c https://repo.anaconda.com/miniconda/Miniconda3-py39_4.11.0-Linux-x86_64.
 bash Miniconda3-py39_4.11.0-Linux-x86_64.sh -b
 rm -rf Miniconda3-py39_4.11.0-Linux-x86_64.sh
 export PATH=/root/miniconda3/bin:$PATH
+cat >> ~/.profile <<EOF
+export PATH=/root/miniconda3/bin:$PATH
+EOF
