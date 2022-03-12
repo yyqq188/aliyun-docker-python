23f9be364e5a2dc9382e47d62e8b8c74e06c344e
ubuntu <ubuntu@localhost.localdomain>
2022-03-12 20:02:27 +0800

    three

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade -y
+apt-get install -y bzip2 gcc git
+apt-get install -y htop screen vim wget
+apt-get upgrade -y bash
 apt-get clean
 
 wget -c https://repo.anaconda.com/miniconda/Miniconda3-py39_4.11.0-Linux-x86_64.sh
 bash Miniconda3-py39_4.11.0-Linux-x86_64.sh -b
 rm -rf Miniconda3-py39_4.11.0-Linux-x86_64.sh
 export PATH=/root/miniconda3/bin:$PATH
-cat >> ~/.profile <<EOF
-export PATH=/root/miniconda3/bin:$PATH
-EOF
